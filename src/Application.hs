81abb619e0373ef13b08e3b1f47a8bd4775f53b7
Felipe Ramos <feliperc_7@hotmail.com>
2020-11-14 10:31:35 -0300

    Commit 21

--- src/Application.hs
+++ src/Application.hs
@@ -41,7 +41,6 @@ import Handler.Home
 import Handler.Sorvete
 import Handler.Calda
 import Handler.Acompanhamento
-
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
 -- comments there for more details.
