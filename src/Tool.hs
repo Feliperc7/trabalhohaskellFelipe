50afb472a9fa0f8373029633ae9e9667c6f5ccfb
Felipe Ramos <feliperc_7@hotmail.com>
2020-11-21 09:34:14 -0300

    Criando arquivo hamlet

---
+++ src/Tool.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE UndecidableInstances       #-}
+module Tool where
+
+import Import
+
+formPote :: Form Int
+formPote = renderDivs (areq intField "Quantidade: " (Just 1))
