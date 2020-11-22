4cb162963da12ada7cb5b444a10a251f91032797
Felipe Ramos <feliperc_7@hotmail.com>
2020-11-22 17:08:31 -0300

    Modificando a tabela

--- src/Handler/CompraSorv.hs
+++ src/Handler/CompraSorv.hs
@@ -4,7 +4,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE QuasiQuotes #-}
-module Handler.CompraSorv where
+module Handler.Comprasorv where
 
 import Import
 import Tool
@@ -21,24 +21,32 @@ getListCompraSorvR = do
                  Nothing -> redirect HomeR
                  Just (Entity uid usuario) -> do
                      let sql = "SELECT ??,??,?? FROM usuario \
-                        \ INNER JOIN compraSorv ON compraSorv.usuarioid = usuario.id \
-                        \ INNER JOIN sorvete ON compraSorv.sorveteid = sorvete.id \
+                        \ INNER JOIN comprasorv ON comprasorv.usuarioid = usuario.id \
+                        \ INNER JOIN sorvete ON comprasorv.sorveteid = sorvete.id \
                         \ WHERE usuario.id = ?"
-                     sorvetes <- runDB $ rawSql sql [toPersistValue uid] :: Handler [(Entity Usuario,Entity CompraSorv,Entity Sorvete)]
+                     sorvetes <- runDB $ rawSql sql [toPersistValue uid] :: Handler [(Entity Usuario,Entity Comprasorv,Entity Sorvete)]
                      defaultLayout $ do
                         [whamlet|
-                            <h1>
-                                 SORVETES COMPRADOS POR #{usuarioNome usuario}
-                            <ul>
-                                 $forall (Entity _ _, Entity _ compraSorv, Entity _ sorvete) <- sorvetes
-                                     
-                                         #{sorveteNome sorvete}: #{sorvetePreco sorvete * (fromIntegral (compraSorvPote compraSorv))}
-                        
+                                <body style="background-color:SandyBrown;">
+                             <center><caption> <h1> SORVETES COMPRADOS POR #{usuarioNome usuario}</caption><center><br>
+                          <center><table width="60%" style="background-color:black; border:2px solid;text-align:center">
+
+                                <thead style="color: white">
+                                   <th><h3>Sorvete</th>
+                                   <th><h3>Preço Total</th>
+
+                                <tbody style="background-color: white">
+                            
+                                 $forall (Entity _ _, Entity _ comprasorv, Entity _ sorvete) <- sorvetes
+
+                                  <td> #{sorveteNome sorvete}</td>
+                                  <td> R$ #{sorvetePreco sorvete * (fromIntegral (comprasorvPote comprasorv))}</td><tr>
+                                  
                         |]
 
 postCompraSorvR :: SorveteId -> Handler Html
 postCompraSorvR sid = do
-    ((resp,_),_) <- runFormPost formPote
+    ((resp,_),_) <- runFormPost formQt
     case resp of
          FormSuccess pote -> do
              sess <- lookupSession "_EMAIL"
@@ -49,7 +57,7 @@ postCompraSorvR sid = do
                       case usuario of
                            Nothing -> redirect HomeR
                            Just (Entity uid _) -> do
-                               runDB $ insert (CompraSorv uid sid pote)
+                               runDB $ insert (Comprasorv uid sid pote)
                                redirect ListCompraSorvR
          _ -> redirect HomeR
 
