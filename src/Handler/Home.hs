8c8edaab5b596f5d23b0be67259fe0b8022291b7
Felipe Ramos <feliperc_7@hotmail.com>
2020-11-25 14:15:43 -0300

    Alterações no menu

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -18,10 +18,10 @@ getHomeR = do
             
                 <center><h1>SISTEMA DA SORVETERIA</center>
                 
-                <marquee><h2>Seja bem-vindo ao sistema da sorveteria - Aqui você pode cadastrar, listar, editar                              e remover sabores de sorvete, caldas e acompanhamentos!!!<br></marquee>
+                <marquee><h2>Seja bem-vindo ao sistema da sorveteria - Aqui temos sorvetes, caldas e acompanhamentos!!!<br></marquee>
 
                 <br><a href="@{UsuarioR}">
-                <input type="button" value="CADASTRO DE USUÁRIO" style="color:blue;font-weight:bold"></a><br>
+                <input type="submit" value="CADASTRO DE USUÁRIO" style="color:blue;font-weight:bold"></a><br>
 
                 $maybe email <- sess
                         <div>
@@ -33,25 +33,25 @@ getHomeR = do
                 <br><img src=@{StaticR img_sorvete_jpg}> 
             
                 <br><a href="@{SorveteR}">                                                                
-                <input type="button" value="CADASTRO DE SORVETES" style="color:black;font-weight:bold"> 
+                <input type="submit" value="CADASTRO DE SORVETES" style="color:black;font-weight:bold"> 
                         
                 <br><a href="@{ListasR}">                                                                 
-                <input type="button" value="LISTA DOS SORVETES" style="color:black;font-weight:bold"></a><br> 
+                <input type="submit" value="LISTA DOS SORVETES" style="color:black;font-weight:bold"></a><br> 
       
                 <br><img src=@{StaticR img_calda_jpg}>
 
                 <br><a href="@{CaldaR}">
-                <input type="button" value="CADASTRO DE CALDAS" style="color:red;font-weight:bold">
+                <input type="submit" value="CADASTRO DE CALDAS" style="color:red;font-weight:bold">
 
                 <br><a href="@{ListacR}">
-                <input type="button" value="LISTA DAS CALDAS" style="color:red;font-weight:bold"></a><br> 
+                <input type="submit" value="LISTA DAS CALDAS" style="color:red;font-weight:bold"></a><br> 
                    
                 <br><img src=@{StaticR img_acompanhamento_jpg}>
 
                 <br><a href="@{AcompanhamentoR}">
-                <input type="button" value="CADASTRO DE ACOMPANHAMENTOS" style="color:green;font-weight:bold"> 
+                <input type="submit" value="CADASTRO DE ACOMPANHAMENTOS" style="color:green;font-weight:bold"> 
                         
                 <br><a href="@{ListaaR}">
-                <input type="button" value="LISTA DOS ACOMPANHAMENTOS" style="color:green;font-weight:bold"><br> 
+                <input type="submit" value="LISTA DOS ACOMPANHAMENTOS" style="color:green;font-weight:bold"><br> 
   
         |]
